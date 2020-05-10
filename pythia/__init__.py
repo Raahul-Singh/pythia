8e65d2201a2558d724fcc85e9b96c504892dbcdd
Raahul Singh <raahul.s18@iiits.in>
2020-05-10 13:22:50 +0530

    Adds basic layout.

--- pythia/__init__.py
+++ pythia/__init__.py
@@ -1,3 +1,7 @@
+from pythia import cleaning, eda, learning, net, seo
+
+__all__ = ['cleaning', 'eda', 'learning', 'net', 'seo']
+
 from pkg_resources import get_distribution, DistributionNotFound
 try:
     __version__ = get_distribution(__name__).version
