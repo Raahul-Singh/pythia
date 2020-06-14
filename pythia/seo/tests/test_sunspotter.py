4a15351298f578b8fbefc11e9f22032b3576fe8a
Raahul Singh <raahul.s18@iiits.in>
2020-06-14 16:13:27 +0530

    Updates Sunspotter tests path

--- pythia/seo/tests/test_sunspotter.py
+++ pythia/seo/tests/test_sunspotter.py
@@ -5,7 +5,7 @@ from pythia.seo.sunspotter import Sunspotter
 from sunpy.util import SunpyUserWarning
 
 
-path = Path(__file__).resolve().parent.parent.parent.parent / "data/all_clear"
+path = Path.cwd() / "data/all_clear"
 
 
 @pytest.fixture
