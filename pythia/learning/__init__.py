2abaf19a2434e64d3a10c0eab1f41244e5be1a31
Raahul Singh <rasalghul@phaidra.ai>
2020-08-25 20:31:15 +0530

    Fix isort ordering

--- pythia/learning/__init__.py
+++ pythia/learning/__init__.py
@@ -1,3 +1,3 @@
-from pythia.learning.datamodule import *
+from pythia.learning.transforms import * # isort:skip_file
 from pythia.learning.datasets import *
-from pythia.learning.transforms import *
+from pythia.learning.datamodule import * # isort:skip_file
